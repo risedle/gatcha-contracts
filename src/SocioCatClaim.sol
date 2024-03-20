b3c610668c43738f600f012162d2832a1116ea2e
farreldarian <farreldarian@users.noreply.github.com>
2024-03-20 07:37:59 +0700

    feat: create claim method

--- src/SocioCatClaim.sol
+++ src/SocioCatClaim.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.25;
 
-contract SocioCatClaim {}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract SocioCatClaim {
+    using SafeERC20 for IERC20;
+    IERC20 public token;
+
+    constructor(IERC20 _token) {
+        token = _token;
+    }
+
+    function claim(address to, uint256 amount) external {
+        token.safeTransfer(to, amount);
+    }
+}
