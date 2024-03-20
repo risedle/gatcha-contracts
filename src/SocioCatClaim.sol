097e8618cdc3cc03f6588340c8525d590074df7a
farreldarian <farreldarian@users.noreply.github.com>
2024-03-20 08:43:57 +0700

    feat: check signature

--- src/SocioCatClaim.sol
+++ src/SocioCatClaim.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.25;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 
 contract SocioCatClaim {
     using SafeERC20 for IERC20;
@@ -25,6 +26,16 @@ contract SocioCatClaim {
         bytes calldata signature,
         address receiver
     ) external {
+        if (
+            !SignatureChecker.isValidSignatureNow(
+                signer,
+                keccak256(abi.encodePacked(msg.sender, amount, nonce)),
+                signature
+            )
+        ) {
+            revert InvalidSignature();
+        }
+
         if (receiver == address(0)) {
             receiver = msg.sender;
         }
