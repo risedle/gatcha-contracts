8cdc92a90a9d9a67e6718f17f4e9d78e77e0c511
farreldarian <farreldarian@users.noreply.github.com>
2024-03-20 08:33:42 +0700

    test: add receiver test

--- test/SocioCatClaim.t.sol
+++ test/SocioCatClaim.t.sol
@@ -8,7 +8,6 @@ import {Token} from "../src/mocks/Token.sol";
 contract SocioCatClaimTest is Test {
     SocioCatClaim public claim;
     Token public token;
-
     function setUp() public {
         token = new Token();
         claim = new SocioCatClaim(token);
@@ -19,9 +18,24 @@ contract SocioCatClaimTest is Test {
         token.mint(address(claim), 100);
         // --
 
-        claim.claim(100, vitalik);
+        vm.startPrank(vitalik);
+        claim.claim(100, address(0));
 
         assertEq(token.balanceOf(address(claim)), 0);
         assertEq(token.balanceOf(vitalik), 100);
+    }
+
+    function test_claimToReceiver() public {
+        address vitalik = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
+        address receiver = 0x220866B1A2219f40e72f5c628B65D54268cA3A9D;
+        token.mint(address(claim), 100);
+        // --
+
+        vm.startPrank(vitalik);
+        claim.claim(100, receiver);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(address(claim)), 0);
+        assertEq(token.balanceOf(receiver), 100);
     }
 }
